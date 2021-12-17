e5dec155867e0bb659da845eb11d765181691635
iamtaehoon <kimth9981@naver.com>
2021-12-17 21:28:42 +0900

    docs(): 마무리한 기능 체크

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -7,7 +7,6 @@ import subway.controller.PathCheckController;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
         PathCheckController pathCheckController = new PathCheckController(scanner);
         pathCheckController.run();
     }
